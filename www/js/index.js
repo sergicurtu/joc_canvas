1ee8f82726316ef055a223ae5aaff67096c1e6e3
sergicurtu <sergi.curtu@numancia.cat>
2016-04-27 12:12:15 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,16 @@
 $(document).on('deviceready', function() {
 
 
+      function draw() {
+        var canvas = document.getElementById('canvas');
+        if (canvas.getContext) {
+          var ctx = canvas.getContext('2d');
+      
+          ctx.fillRect(25,25,100,100);
+          ctx.clearRect(45,45,60,60);
+          ctx.strokeRect(50,50,50,50);
+        }
+      }
+
 
 });
