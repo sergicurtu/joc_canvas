2a474db8f9d2b8ef011a4e5859f4181ce59c700d
sergicurtu <sergi.curtu@numancia.cat>
2016-04-27 12:15:21 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,27 @@
 $(document).on('deviceready', function() {
 
 
-      function draw() {
+      var canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d');
+
+      // resize the canvas to fill browser window dynamically
+      window.addEventListener('resize', resizeCanvas, false);
+
+      function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            /**
+             * Your drawings need to be inside this function otherwise they will be reset when 
+             * you resize the browser window and the canvas goes will be cleared.
+             */
+            dibuixar_alguna_cosa(); 
+      }
+      resizeCanvas();
+
+
+
+      function dibuixar_alguna_cosa() {
         var canvas = document.getElementById('canvas');
         if (canvas.getContext) {
           var ctx = canvas.getContext('2d');
