610f7bf30d7069196cf7d09e355c2eec39c5fad0
sergicurtu <sergi.curtu@numancia.cat>
2016-05-20 11:22:36 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -161,3 +161,26 @@ $(document).on('deviceready', function() {
 	}
       
 });
+
+/*
+
+ var pointerEventToXY = function(e){
+      var out = {x:0, y:0};
+      if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+        out.x = touch.pageX;
+        out.y = touch.pageY;
+      } else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
+        out.x = e.pageX;
+        out.y = e.pageY;
+      }
+      return out;
+    };
+    
+    $('a').on('mousedown touchstart', function(e){
+       //console.log(pointerEventToXY(e)); // will return obj ..kind of {x:20,y:40}
+       alert(pointerEventToXY(e));
+    }) 
+
+*/
+
