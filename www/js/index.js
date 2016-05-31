cb9cd9ea8aeac1ba750d0ca96769231c36e224e6
sergicurtu <sergi.curtu@numancia.cat>
2016-05-31 14:01:47 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -23,29 +23,27 @@ $(document).on('deviceready', function() {
 	var posicio_x_bola = centre_x - ( mida_x_bola / 2 ) ;
 	var posicio_y_bola = centre_y - ( mida_y_bola / 2 ) ;
 
-	draw(ctx,amplada_pantalla_CSS,alcada_pantalla_CSS,posicio_x_bola,posicio_y_bola);
+	var nivell = 1 ;
+	
+	draw(ctx,amplada_pantalla_CSS,alcada_pantalla_CSS,posicio_x_bola,posicio_y_bola,mida_x_bola,mida_y_bola,nivell);
  
 
       
 });
 
 
-function draw(ctx,amplada_pantalla_CSS,alcada_pantalla_CSS,posicio_x_bola,posicio_y_bola,mida_x_bola,mida_y_bola) {
+function draw(ctx,amplada_pantalla_CSS,alcada_pantalla_CSS,posicio_x_bola,posicio_y_bola,mida_x_bola,mida_y_bola,nivell) {
 	
 		// dibuixo el fons --> laberint_fons_1.png
 		var img_fons = new Image();   
-		img_fons.onload = function(){
-			ctx.drawImage(img_fons,0,0,amplada_pantalla_CSS,alcada_pantalla_CSS);
-		};
-		img_fons.src = 'img/laberint_fons_1.png'; // Determinar origen
+		if ( nivell==1) {img_fons.src = 'img/laberint_fons_1.png';} ; // Determinar origen
+		ctx.drawImage(img_fons,0,0,amplada_pantalla_CSS,alcada_pantalla_CSS);
 		
-
 		// dibuixo la bola
-		var img = new Image();   // Crear nova imatge
-		img.onload = function(){
-			ctx.drawImage(img,posicio_x_bola,posicio_y_bola,mida_x_bola,mida_y_bola);
-		};
-		img.src = 'img/myImage.png'; // Determinar origen
+		var img = new Image(); 
+		img.src = 'img/myImage.png';
+		ctx.drawImage(img,posicio_x_bola,posicio_y_bola,mida_x_bola,mida_y_bola);
+		
 		
 		window.darrera_posicio_x = posicio_x_bola ;
 		window.darrera_posicio_y = posicio_y_bola ;
@@ -56,5 +54,6 @@ function pausecomp(ms) {
    ms += new Date().getTime();
    while (new Date() < ms){}
 } 
+
 
 
